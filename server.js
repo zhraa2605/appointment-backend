9aae8a61bfb914525a571bd6a0ac1f3b3461a3b8
zhraa2605 <zhraa.e.l.2605@gmail.com>
2025-05-31 12:16:03 +0300

    https set up

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use('/api/notifications', require('./src/routes/notificationroutes'));
 // Server listening on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running `);
   
 });
 
--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -89,8 +89,8 @@ const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,   // For HTTP
-      sameSite: 'lax', // Allows cookies on same-site requests
+      secure: true,   // For HTTP
+      sameSite: 'none', // Allows cookies on same-site requests
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
     
